7f54e6e5a916cf5aa27f3b97e9b06b8433cfeab2
julio castillo <julio castillo>
2017-07-18 05:04:54 -0500

    first form

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+    <title>Prueba calendario - Prosoft</title>
+    </head>
+
+  <body>
+
+  <form>
+	  <table>
+	  	<tr>
+	  		<td>Start date: </td>
+	  		<td><input type="text" name="start-date" id="start-date"></td>
+	  	</tr>
+	  	<tr>
+	  		<td>Number of days: </td>
+	  		<td><input type="text" name="number-days" id="number-days"></td>
+	  	</tr>
+	  	<tr>
+	  		<td>Country Code: </td>
+	  		<td><input type="text" name="country-code" id="country-code"></td>
+	  	</tr>
+	  	<tr>
+	  		<td></td>
+	  		<td><button type="button" id="generate">Generate calendar</button></td>
+	  	</tr>
+	  </table>
+  </form>
+  
+<script type="text/javascript">
+	
+
+</script>
+
+  </body>
+</html>
